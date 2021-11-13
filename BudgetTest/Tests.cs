2c8ca9dceac0a9d725cb530771aa389ede8becb7
Mars <snowfish7@gmail.com>
2021-11-13 22:48:49 +0800

    Init project

---
+++ BudgetTest/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace BudgetTest {
+    [TestFixture]
+    public class Tests {
+
+        [Test]
+        public void Test1() {
+            Assert.True(true);
+        }
+
+    }
+}
